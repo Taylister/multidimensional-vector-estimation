bd860a1b3f53a340b448f1add9ab964573fd8b84
Taylister <track.and.field.c.love1203@gmail.com>
2020-10-15 17:48:03 +0900

    datasets.pyの完成

---
+++ ValidateCode/confirm_dataset.py
@@ -0,0 +1,57 @@
+def main():
+
+    import torch
+    import os
+    from torchvision import datasets, transforms
+    import numpy as np
+    import matplotlib.pyplot as plt
+    from torchvision.utils import save_image,make_grid
+    
+    import sys
+    sys.path.append('../')
+
+    from datasets import ImageDataset
+    from PIL import Image, ImageChops
+    
+    # ================================================
+    # Test of making dataset
+    # ================================================
+
+    # dataset 
+    size = (60,60)
+    trnsfmColor = transforms.Compose([
+        transforms.Resize(size),
+        transforms.ToTensor()
+    ])
+
+    trnsfmGray = transforms.Compose([
+        transforms.Grayscale(),
+        transforms.Resize(size),
+        transforms.ToTensor()
+    ])
+
+    print("loading dataset... (it may take a few minutes)")
+    data_dir = os.path.join("/home/miyazonotaiga/デスクトップ/MyResearch/multidimensional_vector-estimation", 'data')
+  
+    train_dset = ImageDataset(data_dir, trnsfm=trnsfmColor,class_num = 52)
+    #test_dset = ImageDataset(os.path.join(args.data_dir, 'test'), trnsfm, recursive_search=args.recursive_search)
+
+    train_loader = torch.utils.data.DataLoader(train_dset, batch_size=4, shuffle=True)
+
+    
+    import cv2
+    from tqdm import tqdm
+    import time
+
+    for images,labels,vectors in tqdm(train_loader):
+        imgs = torch.cat((images,images), dim=0)
+        print(vectors)
+        imgpath = os.path.join(data_dir, 'step1.png')
+        save_image(imgs, imgpath, nrow=len(images))
+        break
+    
+    
+if __name__ == '__main__':
+    main()
+
+
---
+++ datasets.py
@@ -0,0 +1,108 @@
+import os
+import imghdr
+import numpy as np
+import torch
+
+import torch.utils.data as data
+import torch.nn.functional as F
+
+from utils import Translater
+import pandas as pd
+
+from PIL import Image
+
+class ImageDataset(data.Dataset):
+    def __init__(self, data_dir, class_num,trnsfm=None):
+        super(ImageDataset, self).__init__()
+
+        
+        self.data_dir = os.path.expanduser(data_dir)
+
+        #=========================================================================
+        # image proceccing information by tensor
+        #=========================================================================
+
+        self.transform = trnsfm
+
+        #=========================================================================
+        # setting dirpath to loading the images and condition
+        #=========================================================================
+       
+        self.img_paths = self.__load_imgpaths_from_dir(os.path.join(self.data_dir,"images"))
+        self.vectors_dir = os.path.join(self.data_dir,"vectors")
+
+        #=========================================================================
+        # loading csv file
+        #=========================================================================
+        
+        csvfile_path = os.path.join(self.data_dir,"information.csv")
+        self.char_df = pd.read_csv(csvfile_path) 
+
+        #=========================================================================
+        # setting class num
+        #=========================================================================
+
+        # using these character -> a-z,A-Z
+        # so the number of class is 26 + 26 = 52
+        self.translater = Translater(num_class=52)
+
+        self.CLASS_NUM = class_num
+
+    def __len__(self):
+        return len(self.img_paths)
+
+
+    def __getitem__(self, index, color_format='RGB'):
+        
+        # used for loading target vector 
+        vector_file_name = os.path.basename(self.img_paths[index]).split(".")[0] + ".npy"
+
+        ###############################################################################
+        # Open same name images (real, mask, and inpaint)
+        # (To fit the number of images. Some preprocess make gap of number of images )  
+        ###############################################################################
+
+        image = Image.open(self.img_paths[index])
+        image = image.convert(color_format)
+
+        ##############################################################
+        
+        if self.transform is not None:
+            image = self.transform(image)
+
+        image_name_without_ext = os.path.basename(self.img_paths[index]).split(".")[0]
+        label = self.char_df.query('image_id == @image_name_without_ext').iat[0,1]
+        onehot = self.translater.chr2num(label)
+
+        ##############################################################
+
+        vector_path = os.path.join(self.vectors_dir,vector_file_name)
+        vector = np.load(vector_path)
+        vector = torch.tensor(vector)
+
+        return  image, onehot, vector
+
+
+    def __is_imgfile(self, filepath):
+        filepath = os.path.expanduser(filepath)
+        if os.path.isfile(filepath) and imghdr.what(filepath):
+            return True
+        return False
+
+
+    def __load_imgpaths_from_dir(self, dirpath, walk=False):
+        imgpaths = []
+        dirpath = os.path.expanduser(dirpath)
+        if walk:
+            for (root, _, files) in os.walk(dirpath):
+                for file in files:
+                    file = os.path.join(root, file)
+                    if self.__is_imgfile(file):
+                        imgpaths.append(file)
+        else:
+            for path in os.listdir(dirpath):
+                path = os.path.join(dirpath, path)
+                if self.__is_imgfile(path) == False:
+                    continue
+                imgpaths.append(path)
+        return imgpaths
